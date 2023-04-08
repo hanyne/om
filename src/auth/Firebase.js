a64a1e8f01bb5df4ebe40fcbb2a2344dcce43e8d
hanine Benali <100847640+hanyne@users.noreply.github.com>
2023-04-09 00:46:48 +0200

    Update Firebase.js

--- src/auth/Firebase.js
+++ src/auth/Firebase.js
@@ -9,16 +9,7 @@ import { getStorage } from "firebase/storage"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBgqFpCmUtcBItH4Z33xfXPga5lcVjpTo0",
-  authDomain: "omoma-ac2d1.firebaseapp.com",
-  projectId: "omoma-ac2d1",
-  storageBucket: "omoma-ac2d1.appspot.com",
-  messagingSenderId: "270316552574",
-  appId: "1:270316552574:web:0b1c653f5b0b36e1f0732f",
-  measurementId: "G-Z77THFX6B1"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
